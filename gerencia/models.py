359f5ae5aeebf847f88e2aef579337ef9323fb21
Kauan <kauan.com18kn@gmail.com>
2025-09-18 00:54:48 -0300

    algo ai

--- gerencia/models.py
+++ gerencia/models.py
@@ -6,12 +6,25 @@ class Clientes(models.Model):
     cnpj = models.CharField(max_length=14, unique=True, null=False, blank=False)
     inscricao_minicipal = models.CharField(unique=True, null=False, blank=False)
     inscricao_estadual = models.CharField(unique=True, null=False, blank=False)
+    data_insercao = models.DateTimeField(auto_now_add=True)
+    Ultima_edicao = models.DateTimeField(auto_now=True)
+    endereco = models.OneToOneField(Endereco, models.PROTECT)
+    
+    
+class Endereco(models.Model):
     logradoro = models.CharField(max_length=255)
     complemento = models.CharField(max_length=255)
     bairro = models.CharField(max_length=255)
     municipio = models.CharField(max_length=255)
     uf = models.CharField(max_length=2, null=False, blank=False)
     cep = models.CharField(max_length=8, null=False)
+    telefone = models.CharField(max_length=11)
+    celular = models.CharField(max_length=11)
+    whatsapp = models.BooleanField(default=False)
+    email = models.EmailField(max_length=100)   
+    url = models.CharField(max_length=255)
+    
+class Contato(models,models):
     telefone = models.CharField(max_length=11)
     celular = models.CharField(max_length=11)
     whatsapp = models.BooleanField(default=False)
--- gerencia/views.py
+++ gerencia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from.models import Clientes
@@ -8,19 +8,41 @@ def home(request):
     if request.method == "GET":
         return render(request, 'home.html')
     elif request.method == "POST":
-        razao_social = request.POST.get('razao_social')
-        nome_fantasia= request.POST.get('nome_fantasia')
-        cnpj = request.POST.get('cnpj')
-        ins_estadual = request.POST.get('ins_estatual')
-        ins_municipal = request.POST.get('ins_estatual')
-        lagradouro = request.POST.get('logradouro')
-        complemento = request.POST.get('complemento')
-        bairro = request.POST.get('bairro')
-        minicipio = request.POST.get('municipio')
-        uf = request.POST.get('uf')
-        cep = request.POST.get('cep')
-        num_tell = request.POST.get('num_tell')
-        num_cell = request.POST.get('celular')
-        whatsapp =request.POST.get('whatsapp')
-        email = request.POST.get('email')
-        url = request.POST.get('url')
+            razao_social = request.POST.get('razao_social')
+            nome_fantasia= request.POST.get('nome_fantasia')
+            cnpj = request.POST.get('cnpj')
+            ins_estadual = request.POST.get('ins_estatual')
+            ins_municipal = request.POST.get('ins_estatual')
+            logradouro = request.POST.get('logradouro')
+            complemento = request.POST.get('complemento')
+            bairro = request.POST.get('bairro')
+            municipio = request.POST.get('municipio')
+            uf = request.POST.get('uf')
+            cep = request.POST.get('cep')
+            num_tell = request.POST.get('num_tell')
+            num_cell = request.POST.get('celular')
+            whatsapp =request.POST.get('whatsapp')
+            email = request.POST.get('email')
+            url = request.POST.get('url')        
+            
+            cliente = Clientes(
+                nome_fantasia = nome_fantasia,
+                razao_social = razao_social,
+                cnpj = cnpj,
+                inscricao_minicipal = ins_estadual,
+                inscricao_estadual = ins_municipal,
+                logradoro = logradouro,
+                complemento = complemento,
+                bairro = bairro,
+                municipio = municipio,
+                uf = uf,
+                cep = cep,
+                celular = num_cell,
+                telefone = num_tell,
+                whatsapp = whatsapp,
+                email = email,
+                url = url
+                )
+            return HttpResponse("thanks")
+        
+        
